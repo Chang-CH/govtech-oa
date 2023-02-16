935c1b0c21d2cd11a0615a673472d5b03489a5bd
Chang-CH <37036869+Chang-CH@users.noreply.github.com>
2023-02-16 20:38:08 +0800

    fix: missing path alias

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const commons = {
     alias: {
       _assets: path.resolve(__dirname, 'src/assets/'),
       _constants: path.resolve(__dirname, 'src/constants/'),
+      _styles: path.resolve(__dirname, 'src/styles/'),
       _components: path.resolve(__dirname, 'src/components/'),
     },
     extensions: ['.ts', '.tsx', '.js'],
