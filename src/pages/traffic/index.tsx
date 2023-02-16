bb2e2474da1c7e502c88dda39fba249850ee32a7
Chang-CH <37036869+Chang-CH@users.noreply.github.com>
2023-02-16 13:52:56 +0800

    change: import new components

--- src/pages/traffic/index.tsx
+++ src/pages/traffic/index.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { DatePicker, Image, Space, Table, TimePicker } from 'antd'
+import PageLayout from '_components/PageLayout';
+import styles from './s.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <PageLayout>
+      <Space>
+        <DatePicker/>
+        <TimePicker/>
+      </Space>
+      <Space>
+        <Table/>
+        <div>
+          weather
+        </div>
+      </Space>
+      <Image/>
+    </PageLayout>
   )
 }
 
